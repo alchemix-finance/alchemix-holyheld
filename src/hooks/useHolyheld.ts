08ec9a1a2727fd65249d0597c28eb16ee25207f2
Gérald Quenum <geraldquenum9@gmail.com>
2025-02-06 16:49:57 +0100

    Revert "validation tag"
    This reverts commit fe61a72f37e0cd8f2f9f84ba00fdef577a945067.

--- src/hooks/useHolyheld.ts
+++ src/hooks/useHolyheld.ts
@@ -54,10 +54,7 @@ export const useHolyheldSDK = (): UseHolyheldSDKReturn => {
 
   const validateHolytag = async (holytag: string) => {
     if (!sdk) throw new Error('SDK not initialized');
-    const trimmedTag = holytag.trim();
-    const response = await sdk.offRamp.getTagInfoForTopUp(trimmedTag);
-    console.log("Response from getTagInfoForTopUp for holytag", trimmedTag, response);
-    return response.found;
+    return (await sdk.offRamp.getTagInfoForTopUp(holytag)).found;
   };
 
   const convertToEUR = async (
